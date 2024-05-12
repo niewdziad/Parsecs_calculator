2821babe9b3fd79e814a7256f05036449bc56d0e
niewdziad <142939906+niewdziad@users.noreply.github.com>
2024-05-12 23:41:25 +0200

    Create parsecs_to_lightyears_calculator.py

---
+++ parsecs_to_lightyears_calculator.py
@@ -0,0 +1,5 @@
+parsecs_input = input("Input number of parsecs:")
+parsecs = int(parsecs_input)
+lightyears = 3.26156 * parsecs
+
+print(parsecs_input + " parsecs is " + str(lightyears) + " lightyears")
